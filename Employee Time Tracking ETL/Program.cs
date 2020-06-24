7f40a6599a16adcdc27a2c583a6ad32027a27435
Daniel Moon <daniel_moon16@yahoo.com>
2020-06-24 09:14:55 -0400

    Initial Blank Project

---
+++ Employee Time Tracking ETL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Time_Tracking_ETL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
