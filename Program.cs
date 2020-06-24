6a0d4255bc97657c37b3eacc73dec119ec9f3bf3
Daniel Moon <daniel_moon16@yahoo.com>
2020-06-24 10:37:48 -0400

    Initial blank project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETT_ETL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
